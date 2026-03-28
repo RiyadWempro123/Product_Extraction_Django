d279a49faf9f8e00cd2fd5acb70d03d69013008e
RiyadWempro123 <rhasan@wempro.com>
2026-03-28 10:30:53 +0600

    update..

--- ExtractData/urls.py
+++ ExtractData/urls.py
@@ -6,7 +6,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("products.urls")),
+    path('home/', include("products.urls")),
+    path("", include("dashboard.urls"))
     
 ]
 if settings.DEBUG:
